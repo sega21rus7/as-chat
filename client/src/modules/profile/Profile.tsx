fce45bf66f8a3373d5a2857960ea96f0dd27b792
sega21rus7 <sega21rus98@mail.ru>
2020-12-19 19:09:30 +0300

    загрузка аватара пока только на клиенте

--- client/src/modules/profile/Profile.tsx
+++ client/src/modules/profile/Profile.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Row, Col, Image } from "antd";
 import EditUserForm from "./EditUserForm";
 
 const Profile: React.FC = () => {
   return (
-    <Row>
-      <Col span={12}>
-        <Image
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-      </Col>
-      <Col span={12}>
-        <EditUserForm />
-      </Col>
-    </Row>
+    <EditUserForm />
   );
 };
 
