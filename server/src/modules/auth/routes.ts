69b2440155fb76744f95cc5e5a2aa49943730fc8
sega21rus7 <sega21rus98@mail.ru>
2021-02-12 14:15:53 +0300

    запуск сервера и авторизация по jwt вынесены в отдельный репозиторий

--- server/src/modules/auth/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login as loginController, register as regController } from "./controllers";
-
-const router = express.Router();
-
-router.use("/login", loginController);
-router.use("/register", regController);
-
-export default router;
