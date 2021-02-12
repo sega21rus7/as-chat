68c6351bbce2573a4f7bb42c1bbc344be2a77528
sega21rus7 <sega21rus98@mail.ru>
2021-02-12 16:32:59 +0300

    валидация также для формы авторизации

--- client/src/modules/auth/components/LoginForm.tsx
+++ client/src/modules/auth/components/LoginForm.tsx
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+interface FormValuesType {
+  login: string,
+  password: string,
+}
+
+const requireMes = "Это обязательное поле";
+
+const validationSchema = yup.object({
+  login: yup.string().required(requireMes),
+  password: yup.string().required(requireMes),
+});
 
 const LoginForm: React.FC = () => {
   const [passwordInputType, setPasswordInputType] = useState("password");
+  const formik = useFormik({
+    initialValues: {
+      login: "",
+      password: "",
+    },
+    onSubmit: values => handleSubmit(values),
+    validationSchema,
+  });
+
+  const handleSubmit = (values: FormValuesType) => {
+    console.log("handleSubmit", values);
+  };
 
   const switchPasswordVisibility = () => {
     setPasswordInputType(prev => prev === "password" ? "text" : "password");
   };
 
   return (
-    <form className="auth-form">
+    <form noValidate className="auth-form" onSubmit={formik.handleSubmit}>
       <div className="auth-form__header">
         <h1 className="auth-form__title">Авторизация</h1>
         <p className="auth-form__subtitle">Пожалуйста войдите в свой аккаунт</p>
@@ -19,14 +45,24 @@ const LoginForm: React.FC = () => {
           <input
             className="input__content auth-form__input"
             type="text"
-            placeholder="Введите логин/e-mail" />
+            name="login"
+            onChange={formik.handleChange}
+            value={formik.values.login}
+            placeholder="Введите логин/e-mail"
+          />
+          <div className="input__error">{formik.errors.login}</div>
         </div>
         <div className="input">
           <input
             className="input__content auth-form__input"
             type={passwordInputType}
             autoComplete="on"
-            placeholder="Введите пароль" />
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            placeholder="Введите пароль"
+          />
+          <div className="input__error">{formik.errors.password}</div>
           <div
             className={passwordInputType === "password" ?
               "input__password-suffix" :
