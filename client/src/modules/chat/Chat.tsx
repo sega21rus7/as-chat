850cfa180443cfb6427c0327a2c9311fce8da2d2
sega21rus7 <sega21rus98@mail.ru>
2021-02-16 12:12:14 +0300

    добавлены иконки прочитанных и отрпавленных сообщений

--- client/src/modules/chat/Chat.tsx
+++ client/src/modules/chat/Chat.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Message from "./components/Message/Message";
 import "./chat.scss";
-import avatar from "./assets/avatar.jpg";
+import avatar from "./assets/img/avatar.jpg";
 
 const Chat: React.FC = () => {
   return (
@@ -10,13 +10,13 @@ const Chat: React.FC = () => {
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic inventore rem perspiciatis perferendis quo placeat, consequuntur, consectetur aspernatur, ullam ipsum. Et porro quo error est iure, itaque voluptate quaerat."
         date={1613459614439}
         avatar={avatar}
-        my={false}
       />
       <Message
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic inventore rem perspiciatis perferendis quo placeat, consequuntur, consectetur aspernatur, ullam ipsum. Et porro quo error est iure, itaque voluptate quaerat."
         date={1613463236787}
         avatar={avatar}
         my={true}
+        hadRead={true}
       />
       {/*<Message
         text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic inventore rem perspiciatis perferendis quo placeat, consequuntur, consectetur aspernatur, ullam ipsum. Et porro quo error est iure, itaque voluptate quaerat."
--- client/src/modules/chat/components/Message/Message.tsx
+++ client/src/modules/chat/components/Message/Message.tsx
@@ -3,15 +3,18 @@ import React from "react";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import ruLocale from "date-fns/locale/ru";
 import "./message.scss";
+import hadReadIcon from "./assets/svg/had_read.svg";
+import hadSendIcon from "./assets/svg/had_send.svg";
 
 interface PropsType {
   text: string;
   date: number | Date;
   avatar: string;
-  my: boolean;
+  my?: boolean;
+  hadRead?: boolean;
 }
 
-const Message: React.FC<PropsType> = ({ text, date, avatar, my }) => {
+const Message: React.FC<PropsType> = ({ text, date, avatar, my, hadRead }) => {
   return (
     <div className={my ? "message message_my" : "message"}>
       {!my &&
@@ -25,8 +28,15 @@ const Message: React.FC<PropsType> = ({ text, date, avatar, my }) => {
             {text}
           </div>
         </div>
-        <div className="message__date">
-          {formatDistanceToNow(date, { addSuffix: true, locale: ruLocale })}
+        <div className="message__footer">
+          <span className="message__date">
+            {formatDistanceToNow(date, { addSuffix: true, locale: ruLocale })}
+          </span>
+          {my &&
+            <span className="message__status">
+              <img src={hadRead ? hadReadIcon : hadSendIcon} alt="" />
+            </span>
+          }
         </div>
       </div>
     </div>
