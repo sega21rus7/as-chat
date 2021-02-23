f28dedc55778b31c4b43de6768ad300e0414da7a
sega21rus7 <sega21rus98@mail.ru>
2021-02-23 14:54:17 +0300

    поправлен gitignore

---
+++ client/src/App/App.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { getToken } from "tools";
+import { useSelector } from "tools/hooks";
+import BaseRouter from "../routes";
+import { fetchUser } from "store/auth/actionCreators";
+
+const App: React.FC = () => {
+  const token = !!getToken();
+  const user = useSelector(state => state.auth.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!user && token) {
+      dispatch(fetchUser());
+    }
+  });
+
+  return (
+    <Router>
+      <BaseRouter />
+    </Router>
+  );
+};
+
+export default App;
