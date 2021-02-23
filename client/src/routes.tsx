15b71baf3b902519fdd80507ee057929948d5133
sega21rus7 <sega21rus98@mail.ru>
2021-02-23 12:45:08 +0300

    разделение роутов для авторизованных и нет

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,17 +2,25 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Auth from "modules/auth/Auth";
 import Chat from "modules/chat/Chat";
+import { getToken } from "tools";
 
 const BaseRouter: React.FC = () => {
-  return (
-    <React.Fragment>
+  const isAuth = !!getToken();
+
+  if (isAuth) {
+    return (
       <Switch>
-        <Route exact path="/login" render={() => <Auth />} />
-        <Route exact path="/registration" render={() => <Auth isReg />} />
         <Route exact path="/im" component={Chat} />
         <Redirect to="/im" />
       </Switch>
-    </React.Fragment>
+    );
+  }
+  return (
+    <Switch>
+      <Route exact path="/login" render={() => <Auth />} />
+      <Route exact path="/registration" render={() => <Auth isReg />} />
+      <Redirect to="/login" />
+    </Switch>
   );
 };
 
