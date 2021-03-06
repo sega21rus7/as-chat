d894a26491a8a2651ddd47daf943c6aa9a44da35
sega21rus7 <sega21rus98@mail.ru>
2021-03-06 15:38:39 +0300

    лишний код, роутер сам редиректит

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { BrowserRouter as Router, useHistory } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { getToken } from "tools";
 import { useSelector } from "tools/hooks";
 import BaseRouter from "../routes";
@@ -9,16 +9,11 @@ import { fetchUser } from "store/auth/actionCreators";
 const App: React.FC = () => {
   const token = !!getToken();
   const user = useSelector(state => state.auth.user);
-  const error = useSelector(state => state.auth.fetchUserError);
-  const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (token && !user) {
       dispatch(fetchUser());
-      if (error) {
-        history.push("/login");
-      }
     }
   });
 
